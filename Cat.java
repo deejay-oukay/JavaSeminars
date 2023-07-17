9029e0782cb1f719287c10942f2ef27167d55f1e
Konstantin <dj-ok@mail.ru>
2023-07-17 23:57:24 +0500

    ДЗ 6 (после просмотра семинара)

--- Cat.java
+++ Cat.java
@@ -1,10 +1,19 @@
+import java.util.LinkedList;
+
 public class Cat {
-    private int positionX = 0;
-    private int positionY = 0;
-    private int positionZ = 0;
+    private int positionX;
+    private int positionY;
+    private int positionZ;
     private String petsname;
     static public int maxSpeed = 20;
     static public int maxHeight = 5;
+    private int weigth;
+    private int height;
+    private Boolean sex;
+    private LinkedList<String> vaccinations;
+    private String[] diseases;
+    private String[] awards;
+    private String breed;
 
     public Cat(String name, int startX, int startY, int startZ) {
         this.petsname = name;
@@ -28,5 +37,30 @@ public class Cat {
     public void Status(String action) {
         System.out.printf("%s - %s. x: %d, y: %d, z: %d\n", this.petsname, action, this.positionX, this.positionY,
                 this.positionZ);
+    }
+
+    public boolean vaccinationExists(String name) {
+        try {
+            if (this.vaccinations.contains(name))
+                return (true);
+        } catch (Exception e) {
+            System.out.printf("Что-то не так с проверкой на наличие привики '%s' у кота %s\n", name, this.petsname);
+            return (false);
+        }
+        return (false);
+    }
+
+    public void vaccinationAdd(String name) {
+        try {
+            this.vaccinations.add(name);
+            System.out.printf("Прививка '%s' для кота %s сделана\n", name, this.petsname);
+        } catch (Exception e) {
+            System.out.printf("Кот %s не дал поставить прививку '%s'\n", this.petsname, name);
+        }
+    }
+
+    public void vaccinationsList() {
+        System.out.printf("Прививки кота %s:\n", this.petsname);
+        System.out.println(this.vaccinations);
     }
 }
--- HomeWork6.java
+++ HomeWork6.java
@@ -6,8 +6,14 @@ public class HomeWork6 {
         Cat Tom = new Cat("Tom", 0, 0, 0);
         Tom.Walk(rnd.nextInt(Cat.maxSpeed), rnd.nextInt(Cat.maxSpeed));
         Tom.Jump(rnd.nextInt(Cat.maxHeight));
+
         Cat Felix = new Cat("Felix", 100, 100, -10);
         Felix.Walk(rnd.nextInt(Cat.maxSpeed), rnd.nextInt(Cat.maxSpeed));
         Felix.Jump(rnd.nextInt(Cat.maxHeight));
+
+        Tom.vaccinationsList();
+        if (!Tom.vaccinationExists("Какая-то прививка"))
+            Tom.vaccinationAdd("Какая-то прививка");
+        Tom.vaccinationsList();
     }
 }
